1645346361c7cccbecf3081c1c9f85d430d31891
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-04-29 16:50:25 +0200

    use real data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 import initSqlJs from "sql.js"; // https://sql.js.org/#%2F=     test env https://sql.js.org/examples/GUI/index.html
+import pf from 'pareto-frontier';
 
 // Required to let webpack 4 know it needs to copy the wasm file to our assets
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
@@ -38,6 +39,15 @@ export default function App() {
   else return <SQLRepl db={db} />;
 }
 
+function to_dict(arr){
+  let dico = {}
+  for(let i = 0; i < arr.length; i++){ 
+      dico[arr[i]] = i;
+  }
+  return dico
+}
+const arrayColumn = (arr, n) => arr.map(x => x[n]);
+
 /**
  * A simple SQL read-eval-print-loop
  * @param {{db: import("sql.js").Database}} props
@@ -59,23 +69,153 @@ function SQLRepl({ db }) {
       setResults([]);
     }
   }
-
-
-
-  var time_per_test = [1, 2, 3, 4, 5]
-  var percentage = [1, 6, 3, 6, 1]
-  var eligible_points = [1, 2, 10, 6, 1]
-  var text = ['Dummy-data', 'A-2', 'A-3', 'A-4', 'A-5']
-  var pareto_time_per_test = [1.5, 2.5, 3.5, 4.5, 5.5]
-  var pareto_percentage = [4, 1, 7, 1, 4]
-
+  var df;
   console.log(results)
-  if (results.length> 0) {
-    console.log(results[0])
-    console.log(results[0]['time_per_test'])
+  if (results.length==0) {
+    df = {
+        "columns": [
+            "index",
+            "time",
+            "score",
+            "eligible_points",
+            "percentage",
+            "eligible_tests",
+            "time_per_test"
+        ],
+        "values": [
+            [
+                "baseline_random",
+                1.0000317096710205,
+                3.1298593441845237,
+                18,
+                0.173881074676918,
+                12,
+                0.08333597580591838
+            ],
+            [
+                "saabas",
+                0.10822415351867676,
+                6.035704433917999,
+                11,
+                0.5487004030834545,
+                6,
+                0.01803735891977946
+            ],
+            [
+                "tree_shap",
+                0.08579039573669434,
+                8.256456856690342,
+                11,
+                0.7505869869718492,
+                6,
+                0.014298399289449057
+            ],
+            [
+                "archipelago",
+                1.1139461994171143,
+                4,
+                5,
+                0.8,
+                5,
+                0.22278923988342286
+            ],
+            [
+                "shapley_taylor_interaction",
+                9.025943517684937,
+                3.2907431753151344,
+                5,
+                0.6581486350630269,
+                5,
+                1.8051887035369873
+            ],
+            [
+                "shap_interaction",
+                169.72328877449036,
+                1.0155440414507773,
+                5,
+                0.20310880829015546,
+                5,
+                33.944657754898074
+            ],
+            [
+                "anova",
+                49.0090274810791,
+                1.133826538897581,
+                5,
+                0.2267653077795162,
+                5,
+                9.80180549621582
+            ],
+            [
+                "sage",
+                103.58940887451172,
+                5.019767485198745,
+                7,
+                0.7171096407426779,
+                7,
+                14.798486982073102
+            ],
+            [
+                "lime",
+                825.8491969108582,
+                10.661781883892203,
+                13,
+                0.8201370679917079,
+                7,
+                117.97845670155117
+            ],
+            [
+                "kernel_shap",
+                50.426323652267456,
+                10.133078100329183,
+                11,
+                0.9211889182117439,
+                6,
+                8.404387275377909
+            ],
+            [
+                "maple",
+                966.3937382698059,
+                9.22704081632653,
+                13,
+                0.7097723704866562,
+                7,
+                138.05624832425798
+            ]
+        ]
+    };
   } else {
-
+    console.log(results[0]);
+    df = results[0];
   }
+  // var time_per_test = [1, 2, 3, 4, 5]
+  // var percentage = [1, 6, 3, 6, 1]
+  // var eligible_points = [1, 2, 10, 6, 1]
+  // var text = ['Dummy-data', 'A-2', 'A-3', 'A-4', 'A-5']
+  // var pareto_time_per_test = [1.5, 2.5, 3.5, 4.5, 5.5]
+  // var pareto_percentage = [4, 1, 7, 1, 4]
+
+  // console.log(results)
+  // if (results.length> 0) {
+  var column = to_dict(df.columns)
+  var time_per_test = arrayColumn(df.values, column['time_per_test']);
+  var percentage = arrayColumn(df.values, column['percentage']);
+  var eligible_points = arrayColumn(df.values, column['eligible_points']);
+  var text = arrayColumn(df.values, column['index']);
+
+
+  var merged = []
+  console.log(time_per_test.length)
+  for (let i = 0; i < time_per_test.length; i++) {
+    merged.push([percentage[i], time_per_test[i]]);
+  }
+  console.log(merged);
+  const pareto = pf.getParetoFrontier(merged, { optimize: 'bottomRight' });
+
+  var pareto_time_per_test = arrayColumn(pareto, 1);
+  var pareto_percentage = arrayColumn(pareto, 0);
+
+  // }
 
 
   var trace1 = {
@@ -110,7 +250,8 @@ function SQLRepl({ db }) {
   var layout = {
     xaxis: {
       // range: [ 0.75, 5.25 ],
-      // log_x=True, todo
+      type: 'log',
+      autorange: true,
       title: {
         text: 'Average Time per test [Seconds] ↓'
       }
@@ -161,6 +302,7 @@ function SQLRepl({ db }) {
       <Plot
         data={data}
         layout={layout}
+        divId={'fig'}
       />
     </div>
   );
