f8fc7077ec368350574c60d31324f78c2c10b465
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-04-29 12:21:29 +0200

    first sql query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
-import initSqlJs from "sql.js";
+import initSqlJs from "sql.js"; // https://sql.js.org/#%2F=     test env https://sql.js.org/examples/GUI/index.html
 
 // Required to let webpack 4 know it needs to copy the wasm file to our assets
 import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
+
+import Plot from 'react-plotly.js';
 
 if (window.location.href.includes("localhost")) {
   console.log('localhost');
@@ -48,6 +50,7 @@ function SQLRepl({ db }) {
     try {
       // The sql is executed synchronously on the UI thread.
       // You may want to use a web worker here instead
+      sql = "select * From summary";
       setResults(db.exec(sql)); // an array of objects is returned
       setError(null);
     } catch (err) {
@@ -56,6 +59,84 @@ function SQLRepl({ db }) {
       setResults([]);
     }
   }
+
+
+
+  var time_per_test = [1, 2, 3, 4, 5]
+  var percentage = [1, 6, 3, 6, 1]
+  var eligible_points = [1, 2, 10, 6, 1]
+  var text = ['Dummy-data', 'A-2', 'A-3', 'A-4', 'A-5']
+  var pareto_time_per_test = [1.5, 2.5, 3.5, 4.5, 5.5]
+  var pareto_percentage = [4, 1, 7, 1, 4]
+
+  console.log(results)
+  if (results.length> 0) {
+    console.log(results[0])
+    console.log(results[0]['time_per_test'])
+  } else {
+
+  }
+
+
+  var trace1 = {
+    x: time_per_test,
+    y: percentage,
+    mode: 'markers+text',
+    type: 'scatter',
+    name: 'Explainers',
+    text: text,
+    textposition: 'top center',
+    textfont: {
+      family:  'Raleway, sans-serif'
+    },
+    marker: { size: eligible_points }
+  };
+  
+  var trace2 = {
+    x: pareto_time_per_test,
+    y: pareto_percentage,
+    mode: 'lines',
+    type: 'scatter',
+    name: 'Pareto front',
+    // text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
+    textfont : {
+      family:'Times New Roman'
+    },
+    textposition: 'bottom center',
+    // marker: { size: 6 }
+  };
+  var data = [ trace1, trace2 ];
+  
+  var layout = {
+    xaxis: {
+      // range: [ 0.75, 5.25 ],
+      // log_x=True, todo
+      title: {
+        text: 'Average Time per test [Seconds] ↓'
+      }
+    },
+    yaxis: {
+      // range: [0, 100], todo
+      title: {
+        text: 'Score [%] ↑'
+      }
+    },
+    legend: {
+      y: 1,
+      yref: 'paper',
+      font: {
+        family: 'Arial, sans-serif',
+        // size: 20,
+        // color: 'grey',
+      }
+    },
+    title:"Global overview of the explainers' performance"
+  };
+  
+  // Plotly.newPlot('myDiv', data, layout);
+
+  
+
 
   return (
     <div className="App">
@@ -76,6 +157,11 @@ function SQLRepl({ db }) {
           ))
         }
       </pre>
+      
+      <Plot
+        data={data}
+        layout={layout}
+      />
     </div>
   );
 }
