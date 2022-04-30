48541c7bb7857550b17b34b58d1d1c1c7cdd07d9
Karim-53 <mohamedkarim.belaid@supcom.tn>
2022-05-01 00:45:07 +0200

    fix first execution

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import "./styles.css";
 import initSqlJs from "sql.js"; // https://sql.js.org/#%2F=     test env https://sql.js.org/examples/GUI/index.html
 import pf from 'pareto-frontier';
 import DB from './database';
-// Required to let webpack 4 know it needs to copy the wasm file to our assets
-import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm";
-
+import sqlWasm from "!!file-loader?name=sql-wasm-[contenthash].wasm!sql.js/dist/sql-wasm.wasm"; // Required to let webpack 4 know it needs to copy the wasm file to our assets
 import Plot from 'react-plotly.js';
 // import DataTable from 'react-data-table-component'; todo [after acceptance] https://datatables.net/
-
+function average(data) {
+  /*Can't find an average function in JS, made one
+  This function is able to handle null values!!!*/
+    var count = null;
+    var sum = null;
+    for (let i=0; i<data.length; i++) {
+      if (data[i]) {
+        count++;
+        sum = sum + data[i];
+      }
+    }
+    if (count) {var average = Number( sum / count)} else { average=null}
+    return average;
+  }
 if (window.location.href.includes("localhost")) {
   console.log('localhost');
 }
@@ -43,15 +54,16 @@ export default function App() {
 
 function to_dict(arr){
   let dico = {}
-  for(let i = 0; i < arr.length; i++){ 
+  for(let i = 0; i < arr.length; i++){
       dico[arr[i]] = i;
   }
   return dico
 }
 const arrayColumn = (arr, n) => arr.map(x => x[n]);
+const average_score = (arr, dico) => arr.map(x => average([x[dico['score_fidelity']],x[dico['score_stability']]]));
 
 function plotly_click(data){
-  
+
   console.log('plotly_click:')
   console.log(data)
   console.log(data.points)
@@ -67,163 +79,46 @@ function plotly_click(data){
   // result = db.exec(sql)
 
 }
-function sql_exec(sql) {
-  try {
-    // The sql is executed synchronously on the UI thread.
-    // You may want to use a web worker here instead
-    sql = `SELECT explainer, AVG(score)*100.0 AS percentage, AVG(time) AS time_per_test, count(score) AS eligible_points
-    FROM cross_tab
-    GROUP BY explainer;`;
-    setResults(db.exec(sql)); // an array of objects is returned
-    setError(null);
-  } catch (err) {
-    // exec throws an error when the SQL statement is invalid
-    setError(err);
-    setResults([]);
-  }
-}
 /**
  * A simple SQL read-eval-print-loop
  * @param {{db: import("sql.js").Database}} props
  */
 function SQLRepl({ db }) {
+  const sql = `SELECT	explainer,
+                    AVG(time) AS time_per_test,
+                    count(score) AS eligible_points,
+                    AVG(case category when 'fidelity' then score end)*100.0 AS score_fidelity,
+                    AVG(case category when 'stability' then score end)*100.0 AS score_stability
+            FROM cross_tab
+            Left JOIN test ON cross_tab.test = test.test
+            GROUP BY explainer;`;
   const [error, setError] = useState(null);
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState(db.exec(sql));
 
-
-  var df;
-  // console.log(results)
-  if (results.length==0) {
-    df = {
-        "columns": [
-            "index",
-            "time",
-            "score",
-            "eligible_points",
-            "percentage",
-            "eligible_tests",
-            "time_per_test"
-        ],
-        "values": [
-            [
-                "baseline_random",
-                1.0000317096710205,
-                3.1298593441845237,
-                18,
-                0.173881074676918,
-                12,
-                0.08333597580591838
-            ],
-            [
-                "saabas",
-                0.10822415351867676,
-                6.035704433917999,
-                11,
-                0.5487004030834545,
-                6,
-                0.01803735891977946
-            ],
-            [
-                "tree_shap",
-                0.08579039573669434,
-                8.256456856690342,
-                11,
-                0.7505869869718492,
-                6,
-                0.014298399289449057
-            ],
-            [
-                "archipelago",
-                1.1139461994171143,
-                4,
-                5,
-                0.8,
-                5,
-                0.22278923988342286
-            ],
-            [
-                "shapley_taylor_interaction",
-                9.025943517684937,
-                3.2907431753151344,
-                5,
-                0.6581486350630269,
-                5,
-                1.8051887035369873
-            ],
-            [
-                "shap_interaction",
-                169.72328877449036,
-                1.0155440414507773,
-                5,
-                0.20310880829015546,
-                5,
-                33.944657754898074
-            ],
-            [
-                "anova",
-                49.0090274810791,
-                1.133826538897581,
-                5,
-                0.2267653077795162,
-                5,
-                9.80180549621582
-            ],
-            [
-                "sage",
-                103.58940887451172,
-                5.019767485198745,
-                7,
-                0.7171096407426779,
-                7,
-                14.798486982073102
-            ],
-            [
-                "lime",
-                825.8491969108582,
-                10.661781883892203,
-                13,
-                0.8201370679917079,
-                7,
-                117.97845670155117
-            ],
-            [
-                "kernel_shap",
-                50.426323652267456,
-                10.133078100329183,
-                11,
-                0.9211889182117439,
-                6,
-                8.404387275377909
-            ],
-            [
-                "maple",
-                966.3937382698059,
-                9.22704081632653,
-                13,
-                0.7097723704866562,
-                7,
-                138.05624832425798
-            ]
-        ]
-    };
-  } else {
-    // console.log(results[0]);
-    df = results[0];
+  function sql_exec(sql_bof) {
+    try {
+      // The sql is executed synchronously on the UI thread. You may want to use a web worker here instead
+      setResults(db.exec(sql)); // an array of objects is returned
+      setError(null);
+    } catch (err) {
+      // exec throws an error when the SQL statement is invalid
+      setError(err);
+      setResults([]);
+    }
   }
-  // var time_per_test = [1, 2, 3, 4, 5]
-  // var percentage = [1, 6, 3, 6, 1]
-  // var eligible_points = [1, 2, 10, 6, 1]
-  // var text = ['Dummy-data', 'A-2', 'A-3', 'A-4', 'A-5']
-  // var pareto_time_per_test = [1.5, 2.5, 3.5, 4.5, 5.5]
-  // var pareto_percentage = [4, 1, 7, 1, 4]
 
-  // if (results.length> 0) {
-  var column = to_dict(df.columns)
-  var time_per_test = arrayColumn(df.values, column['time_per_test']);
-  var percentage = arrayColumn(df.values, column['percentage']);
-  var eligible_points = arrayColumn(df.values, column['eligible_points']);
-  var text = arrayColumn(df.values, column['index']);
+  console.log(results)
+  var df;
+  df = results[0];
+  var column = to_dict(df.columns);
 
+  //const score_fidelity = arrayColumn(df.values, column['score_fidelity']);
+  const percentage = average_score(df.values, column)
+  // const percentage = arrayColumn(df.values, column['percentage']);
+  const time_per_test = arrayColumn(df.values, column['time_per_test']);
+  const eligible_points = arrayColumn(df.values, column['eligible_points']);
+  const text = arrayColumn(df.values, column['index']);
+  console.log(df)
 
   var merged = []
   for (let i = 0; i < time_per_test.length; i++) {
@@ -231,8 +126,8 @@ function SQLRepl({ db }) {
   }
   const pareto = pf.getParetoFrontier(merged, { optimize: 'bottomRight' });
 
-  var pareto_time_per_test = arrayColumn(pareto, 1);
-  var pareto_percentage = arrayColumn(pareto, 0);
+  const pareto_time_per_test = arrayColumn(pareto, 1);
+  const pareto_percentage = arrayColumn(pareto, 0);
 
   // }
 
@@ -250,7 +145,7 @@ function SQLRepl({ db }) {
     },
     marker: { size: eligible_points }
   };
-  
+
   var trace2 = {
     x: pareto_time_per_test,
     y: pareto_percentage,
@@ -265,7 +160,7 @@ function SQLRepl({ db }) {
     // marker: { size: 6 }
   };
   var data = [ trace1, trace2 ];
-  
+
   var layout = {
     xaxis: {
       // range: [ 0.75, 5.25 ],
@@ -290,13 +185,13 @@ function SQLRepl({ db }) {
         // color: 'grey',
       }
     },
-    
+
     title:"Global overview of the explainers' performance"
   };  // todo [after acceptance] autosize: true, https://dev.to/dheerajmurali/building-a-responsive-chart-in-react-with-plotly-js-4on8
-  
+
   // Plotly.newPlot('myDiv', data, layout);
 
-  
+
 
 
   return (
@@ -324,7 +219,7 @@ function SQLRepl({ db }) {
           ))
         }
       </pre>
-      
+
       <h1>Click on an explainer for more details</h1>
     </div>
   );
@@ -384,3 +279,16 @@ function ResultsTable({ columns, values }) {
 // Left JOIN test ON cross_tab.test = test.test
 // Where explainer = 'baseline_random'
 // GROUP BY category
+
+
+// SELECT	explainer,
+// 		AVG(time) AS time_per_test,
+//  		count(score) AS eligible_points,
+// 		AVG(case category when 'fidelity' then score end) AS score_fidelity,
+// 		AVG(case category when 'stability' then score end) AS score_stability
+// FROM cross_tab
+// Left JOIN test ON cross_tab.test = test.test
+// GROUP BY explainer
+
+
+// (IFNULL(score_fidelity, 0.0)+IFNULL(score_stability, 0.0))/
